a71b2e9fff2c2f37f131476ab384ff2d6b513883
ynghan <119781387+ynghan@users.noreply.github.com>
2024-01-04 12:50:27 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 54996 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+def test_problem_queue(num):
+    deq = deque([i for i in range(1, num + 1)])
+    while len(deq) > 1:
+        deq.popleft()
+        deq.append(deq.popleft())
+    return deq.popleft()
+
+N = int(input())
+result = test_problem_queue(N)
+print(result)
