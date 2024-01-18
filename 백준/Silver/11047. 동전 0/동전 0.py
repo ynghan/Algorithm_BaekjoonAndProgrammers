eed4b43da30f94728708181424cdb93e8842975a
ynghan <119781387+ynghan@users.noreply.github.com>
2024-01-18 11:36:27 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,3 @@
-# S4 11047
-# https://www.acmicpc.net/problem/11047
-
-"""
-동전의 종류 N
-몇 가지 동전의 합 K
-
-출력 : 이때 필요한 동전 개수의 최솟값
-"""
 class Solution():
     def coin(self):
         N, K = map(int, input().rstrip().split())
@@ -15,17 +6,19 @@ class Solution():
         for i in range(N):
             coin.append(int(input()))
 
-        for i in range(0, len(coin)-1, 1):
-            div = coin[i+1] % coin[i]
-            maximum = coin[i+1] // coin[i] - 1
-            if div == 0:
-                max.append(maximum)
+        for i in range(0, len(coin), 1):
+            if i < len(coin) - 1:
+                div = coin[i+1] % coin[i]
+                maximum = coin[i+1] // coin[i] - 1
+                if div == 0:
+                    max.append(maximum)
+                else:
+                    max.append(0)
             else:
-                max.append(0)
-
-        max.append(K // coin[-1])
+                max.append(K // coin[-1])
 
         res = 0
+
         while K > 0:
             for i in range(len(coin)-1, -1, -1):
                 if K >= coin[i]:
@@ -38,5 +31,3 @@ class Solution():
 
 a = Solution()
 a.coin()
-
-
