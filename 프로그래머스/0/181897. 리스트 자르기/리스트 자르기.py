87d70ec910d027b36718954e5c690e4dbf3bdc0a
ynghan <119781387+ynghan@users.noreply.github.com>
2024-04-18 09:53:04 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,12 @@
+def solution(n, slicer, num_list):
+    answer = []
+    
+    if n == 1:
+        return num_list[:slicer[1]+1]
+    elif n == 2:
+        return num_list[slicer[0]:]
+    elif n == 3:
+        return num_list[slicer[0]:slicer[1]+1]
+    elif n == 4:
+        return num_list[slicer[0]:slicer[1]+1:slicer[2]]    
+    return answer
