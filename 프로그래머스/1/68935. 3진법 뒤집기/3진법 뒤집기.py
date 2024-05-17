80c95628f7a0f765a4509d8a2d251135c23a166d
ynghan <119781387+ynghan@users.noreply.github.com>
2024-05-17 10:12:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,25 +1,3 @@
 def solution(n):
-    answer = []
-    result = 0
-    
-    # 1. 3진수 표현 방법  # 1 3 9 27 81
-    
-    i = 0
-    while n > 0:
-        val = n // 3
-        mod = n % 3
-        answer.append(mod)
-        n = val
-    
-
-    print(answer)
-    
-    # 10진법으로 표현
-    for i in range(len(answer)):
-        result += answer[i]*(3**(len(answer)-1-i))
-    
-    
-    return result
-
-# 10 = 1010
-
+    answer = 0
+    return answer
