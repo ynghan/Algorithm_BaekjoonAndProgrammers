3bbead1dc65b686c755d4424777ca04a049a632c
ynghan <119781387+ynghan@users.noreply.github.com>
2024-05-22 16:24:38 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    count = 0
+    while num != 1:
+        if count > 500:
+            break
+        elif num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        count += 1
+    
+    return count if count < 500 else -1
