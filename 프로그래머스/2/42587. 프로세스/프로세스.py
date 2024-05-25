a547199c635b9c595da3ff261b30fec59f73f36c
ynghan <119781387+ynghan@users.noreply.github.com>
2024-05-25 15:13:01 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,36 +1,25 @@
-# 특정 프로세스가 몇 번째로 실행되는지 구하기
-
-# 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.
-# 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.
-# 3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.
-# 3.1 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료합니다.
-
-# [2, 1, 3, 2] -> [1, 3, 2, 2] -> [3, 2, 2] -> [2, 2, 3] -> [2, 3] -> [3]
-# [2, 1, 3, 2] -> [1, 3, 2, 2] -> [3, 2, 2, 1] -> [2,2,1] -> [2,1] -> [1] : C D A B
-# -> 큰 숫자가 우선 순위가 높다.
-
-# ex
-# priorities [2,1,3,2] 이고 location은 C인 경우 answer은 1
-
 def solution(priorities, location):
 
-    rank = 1
-    current_idx = location
+    rank = 1 # 일단 이거는 몇 번째로 pop 됐는지 나타내는 변수입니다.
+    current_idx = location # 이거는 현재 몇 번째 인덱스에 있는지 추적하는 인덱스 ㅋㅋ
 
-    while True:
-        num = priorities.pop(0)
-        if priorities:
-            if num < max(priorities):
-                priorities.append(num)
-                if current_idx == 0:
-                    current_idx = len(priorities)-1
-                else:
-                    current_idx -= 1
-            else:
-                if current_idx == 0:
+    while True: # 무한 반복
+        num = priorities.pop(0) # 하나를 pop 합니다.
+        if priorities: # 만약 priorities가 있다면, 이거 안해주니까 [1], 0 이렇게 주어진 경우에 무한 루프 걸리더라고요?
+            if num < max(priorities): # priorities의 최대값과 비교해줍니다.
+                priorities.append(num) # 작으면 다시 넣어줍니다.
+                if current_idx == 0: # 이때 location 값이 0이었다면 
+                    current_idx = len(priorities)-1 # 다시 넣은 인덱스 값으로 변경해줍니다.
+                else: # 0이 아니라면
+                    current_idx -= 1 # 그냥 1 감소 시킵니다.
+            else: # 탈출하는 경우?
+                if current_idx == 0: # location이 0이었다면 해당 값이 몇 번째로 탈출했는지 말하라는 거니까 rank 반환해 줍니다.
                     return rank
-                else:
-                    rank += 1
-                    current_idx -= 1
+                else: # 이거의 순위를 말하라는게 아니니 
+                    rank += 1  # 순위 증가 시키고 다음 반복문 타도록 해줍니다.
+                    current_idx -= 1 # 이거는 0이 아니니까 그냥 1 감소시키면 됩니다.
         else:
             return rank
+
+
+
