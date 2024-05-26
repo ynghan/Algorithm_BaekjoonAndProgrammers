5074705bedc87a26243bcc21248ed81303525196
ynghan <119781387+ynghan@users.noreply.github.com>
2024-05-26 18:15:22 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,11 +1,3 @@
-
-
 def solution(food):
-    half = []
-    for idx, num in enumerate(food):
-        if idx == 0:
-            continue
-        else:
-            for _ in range(num//2):
-                half.append(str(idx))
-    return ''.join(half) + "0" + ''.join(half[::-1])
+    answer = ''
+    return answer
