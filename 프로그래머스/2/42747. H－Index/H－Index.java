ad3801a18966941460c374b4456292c809cfffde
ynghan <119781387+ynghan@users.noreply.github.com>
2024-05-30 09:50:54 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i =0 ; i < citations.length;i++) {
+            int smaller = Math.min(citations[i], citations.length-i);
+            
+            if(smaller >= answer)
+                answer = Math.max(answer, smaller);
+            else
+                break;
+        }
+        return answer;
+    }
+}
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,6 +1,6 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 09:50:02
+2024년 05월 30일 09:50:51
 
 ### 문제 설명
 
