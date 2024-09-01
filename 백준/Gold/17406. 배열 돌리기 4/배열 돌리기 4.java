dc3d6752656eb6868dd3e3a3bd9bf706bb55f669
ynghan <119781387+ynghan@users.noreply.github.com>
2024-09-01 23:17:53 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 264 ms, Memory: 29952 KB -BaekjoonHub

---
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static int[][] rotate;
+    static List<List<Integer>> permutations;
+    static List<Integer> results;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = reader.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        int K = Integer.parseInt(s[2]);
+
+        int[][] board = new int[N + 1][M + 1];
+        rotate = new int[K][3];
+        permutations = new ArrayList<>();
+        results = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            String[] row = reader.readLine().split(" ");
+            for (int j = 1; j <= M; j++) {
+                board[i][j] = Integer.parseInt(row[j - 1]);
+            }
+        }
+
+        for (int i = 0; i < K; i++) {
+            String[] s1 = reader.readLine().split(" ");
+            rotate[i][0] = Integer.parseInt(s1[0]);
+            rotate[i][1] = Integer.parseInt(s1[1]);
+            rotate[i][2] = Integer.parseInt(s1[2]);
+        }
+
+        int[] arr = new int[K];
+        for (int k = 0; k < arr.length; k++) {
+            arr[k] = k;
+        }
+
+        generatePermutations(arr, 0, permutations);
+
+        for (List<Integer> permutation : permutations) {
+            int[][] tempBoard = copyBoard(board);
+            int result = rotate(tempBoard, permutation);
+            results.add(result);
+        }
+
+        System.out.println(Collections.min(results));
+    }
+
+    public static void generatePermutations(int[] rotate, int start, List<List<Integer>> permutations) {
+        if (start == rotate.length) {
+            List<Integer> perm = new ArrayList<>();
+            for (int num : rotate) {
+                perm.add(num);
+            }
+            permutations.add(perm);
+            return;
+        }
+
+        for (int i = start; i < rotate.length; i++) {
+            swap(rotate, start, i);
+            generatePermutations(rotate, start + 1, permutations);
+            swap(rotate, start, i);
+        }
+    }
+
+    public static void swap(int[] rotate, int i, int j) {
+        int temp = rotate[i];
+        rotate[i] = rotate[j];
+        rotate[j] = temp;
+    }
+
+    public static int rotate(int[][] board, List<Integer> permutation) {
+        for (Integer idx : permutation) {
+            int r = rotate[idx][0];
+            int c = rotate[idx][1];
+            int s = rotate[idx][2];
+
+            for (int i = 0; i < s; i++) {
+                int up_left_x = r - s + i;
+                int up_left_y = c - s + i;
+                int down_right_x = r + s - i;
+                int down_right_y = c + s - i;
+
+                int temp = board[up_left_x][up_left_y];
+
+                // Rotate left
+                for (int row = up_left_x; row < down_right_x; row++) {
+                    board[row][up_left_y] = board[row + 1][up_left_y];
+                }
+                // Rotate down
+                for (int col = up_left_y; col < down_right_y; col++) {
+                    board[down_right_x][col] = board[down_right_x][col + 1];
+                }
+                // Rotate right
+                for (int row = down_right_x; row > up_left_x; row--) {
+                    board[row][down_right_y] = board[row - 1][down_right_y];
+                }
+                // Rotate up
+                for (int col = down_right_y; col > up_left_y + 1; col--) {
+                    board[up_left_x][col] = board[up_left_x][col - 1];
+                }
+
+                board[up_left_x][up_left_y + 1] = temp;
+            }
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        for (int row = 1; row <= board.length - 1; row++) {
+            int sum = 0;
+            for (int col = 1; col <= board[row].length - 1; col++) {
+                sum += board[row][col];
+            }
+            minSum = Math.min(minSum, sum);
+        }
+
+        return minSum;
+    }
+
+    public static int[][] copyBoard(int[][] original) {
+        int[][] copy = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = original[i].clone();
+        }
+        return copy;
+    }
+}
