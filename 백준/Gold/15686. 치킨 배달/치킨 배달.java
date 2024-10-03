c8ec613e3e024b0b1c30eed10c77ec1f7d53de3b
ynghan <119781387+ynghan@users.noreply.github.com>
2024-10-03 17:26:25 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -3,62 +3,81 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static List<int[]> houses, chickens;
+    static int N;
+    static int M;
+    static List<Pos> chickens;
+    static List<Pos> houses;
     static int minChickenDis = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        String[] s = reader.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
 
-        houses = new ArrayList<>();
         chickens = new ArrayList<>();
+        houses = new ArrayList<>();
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(reader.readLine());
-            for (int j = 0; j < N; j++) {
-                int val = Integer.parseInt(st.nextToken());
-                if (val == 1) {
-                    houses.add(new int[]{i, j});
-                } else if (val == 2) {
-                    chickens.add(new int[]{i, j});
+        for(int i = 0; i < N ; i++) {
+            String[] s1 = reader.readLine().split(" ");
+            for(int j = 0; j < N; j++) {
+                int val = Integer.parseInt(s1[j]);
+                if(val == 2) {
+                    chickens.add(new Pos(i, j));
+                }
+                else if(val == 1) {
+                    houses.add(new Pos(i,j));
                 }
             }
         }
 
-        combination(new int[M], 0, 0);
+        // 치킨 집 M개 고르기
+        boolean[] visited = new boolean[chickens.size()];
+        chooseM(visited, 0, 0);
+
+        // 최소 치킨 거리 출력하기
         System.out.println(minChickenDis);
     }
 
-    private static void combination(int[] selected, int start, int depth) {
-        if (depth == M) {
-            minChickenDis = Math.min(minChickenDis, calculateTotalDistance(selected));
+    private static void chooseM(boolean[] visited, int start, int depth) {
+        if(depth == M) {
+            // 치킨 거리 합 구하기
+            int sum = calculateChickenDistance(visited);
+            minChickenDis = Math.min(minChickenDis, sum);
             return;
         }
 
-        for (int i = start; i < chickens.size(); i++) {
-            selected[depth] = i;
-            combination(selected, i + 1, depth + 1);
+        for(int i = start; i < chickens.size(); i++) {
+            visited[i] = true;
+            chooseM(visited, i + 1, depth + 1);
+            visited[i] = false;
         }
     }
 
-    private static int calculateTotalDistance(int[] selected) {
+    private static int calculateChickenDistance(boolean[] visited) {
         int totalDistance = 0;
-        for (int[] house : houses) {
+        for (Pos house : houses) {
             int minDistance = Integer.MAX_VALUE;
-            for (int i : selected) {
-                int[] chicken = chickens.get(i);
-                int distance = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-                minDistance = Math.min(minDistance, distance);
+            for (int i = 0; i < chickens.size(); i++) {
+                if (visited[i]) {
+                    Pos chicken = chickens.get(i);
+                    int distance = Math.abs(house.x - chicken.x) + Math.abs(house.y - chicken.y);
+                    minDistance = Math.min(minDistance, distance);
+                }
             }
             totalDistance += minDistance;
-            if (totalDistance >= minChickenDis) return Integer.MAX_VALUE;
         }
         return totalDistance;
+    }
+
+    public static class Pos {
+        int x;
+        int y;
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
