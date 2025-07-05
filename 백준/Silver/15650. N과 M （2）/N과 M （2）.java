e75ff56eef3d28eedb5eed26f4d9eafd71d39ad6
ynghan <119781387+ynghan@users.noreply.github.com>
2025-07-05 17:04:34 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,48 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] s = br.readLine().split(" ");
+    int N = Integer.parseInt(s[0]);
+    int M = Integer.parseInt(s[1]);
+      
+    int[] arr = new int[N];
+    boolean[] visited = new boolean[N];
+    // 초기화
+    for(int i = 0; i < N; i++) {
+      arr[i] = i + 1;
+    }
+    combination(arr, new int[M], 0, 0);
 
-    public static int[] arr;
-    public static boolean[] visited;
+  }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int M = Integer.parseInt(s[1]);
+  public static void combination(int[] arr, int[] output, int start, int depth) {
 
-        arr = new int[M];
-        visited = new boolean[N];
-
-        dfs(N, M, 0);
-
+    if(output.length == depth) {
+      for (int i : output) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+      return;
     }
 
-    public static void dfs(int N, int M, int depth) {
-        if(depth == M) {
-            int temp = arr[0];
-            for(int i = 1 ; i < M; i++) {
-                if(temp > arr[i]) {
-                    return;
-                }
-                else {
-                    temp = arr[i];
-                }
-            }
-            for (int i : arr) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for(int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
-                visited[i] = false;
-            }
-        }
+    for(int i = start; i < arr.length; i++) {
+      output[depth] = arr[i];
+      combination(arr, output,i + 1, depth + 1);
     }
+  }
 }
