5db1deae069ec848d1f594faf913acf91a95e4b3
ynghan <119781387+ynghan@users.noreply.github.com>
2025-07-06 11:14:34 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -3,42 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+  static StringBuilder sb = new StringBuilder();
 
-    public static int[] arr;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] s = br.readLine().split(" ");
+    int N = Integer.parseInt(s[0]);
+    int M = Integer.parseInt(s[1]);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int M = Integer.parseInt(s[1]);
+    int[] arr = new int[N];
+    int[] output = new int[M];
 
-        arr = new int[M];
-
-        dfs(N, M, 0);
-
+    for(int i = 0; i < N; i++) {
+      arr[i] = i+1;
     }
 
-    public static void dfs(int N, int M, int depth) {
-        if(depth == M) {
-            int temp = arr[0];
-            for(int i = 1 ; i < M; i++) {
-                if(temp > arr[i]) {
-                    return;
-                }
-                else {
-                    temp = arr[i];
-                }
-            }
-            for (int i : arr) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            return;
-        }
+    dfs(arr, output, 0, 0);
+    System.out.print(sb.toString());
+  }
 
-        for(int i = 0; i < N; i++) {
-            arr[depth] = i + 1;
-            dfs(N, M, depth + 1);
-        }
+
+  public static void dfs(int[] arr, int[] output, int depth, int min) {
+
+    if(output.length == depth) {
+      for (int i : output) sb.append(i).append(" ");
+      sb.append("\n");
+      return;
     }
+
+    for(int i = min; i < arr.length; i++) {
+      output[depth] = arr[i];
+      dfs(arr, output, depth+1, i);
+    }
+  }
 }
