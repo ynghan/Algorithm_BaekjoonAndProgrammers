6c9044e99b14ebfed4f30355ec269d1487936115
ynghan <119781387+ynghan@users.noreply.github.com>
2025-07-08 13:19:31 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.58 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 84.5 MB, 시간: 5.21 ms
+메모리: 88.1 MB, 시간: 4.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 12:16:09
+2025년 07월 08일 13:19:27
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,29 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        TreeSet<Integer> answer = new TreeSet<>();
-        Arrays.sort(numbers);
-        boolean[] visited = new boolean[numbers.length];
-        int[] output = new int[2];
-        dfs(numbers, visited, output, 0, answer);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    public void dfs(int[] numbers, boolean[] visited, int[] output, int depth, TreeSet<Integer> answer) {
-        
-        if(output.length == depth) {
-            answer.add(output[0] + output[1]);
-            return;
-        }
+        TreeSet<Integer> set = new TreeSet<>();
         
         for(int i = 0; i < numbers.length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                output[depth] = numbers[i];
-                dfs(numbers, visited, output, depth+1, answer);
-                visited[i] = false;
+            for(int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
