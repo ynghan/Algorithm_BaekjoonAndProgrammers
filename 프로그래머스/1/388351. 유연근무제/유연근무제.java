bc69e153f89b69bb579fd0929c6a7777240fa7e1
ynghan <119781387+ynghan@users.noreply.github.com>
2025-07-17 15:25:31 +0900

    [level 1] Title: 유연근무제, Time: 0.58 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,34 +1,24 @@
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
-        int answer = 0;
-
-        for (int i = 0; i < schedules.length; i++) {
-            int hopeMin = toMinutes(schedules[i]);
-            int endMin = hopeMin + 10;
-            boolean allOnTime = true;
-
-            for (int j = 0; j < 7; j++) {
-                int day = (startday + j - 1) % 7 + 1; // 1~7 유지
-
-                if (day >= 1 && day <= 5) { // 평일
-                    int actualMin = toMinutes(timelogs[i][j]);
-
-                    if (actualMin > endMin) {
-                        allOnTime = false;
-                        break;
-                    }
-                }
+        int total = timelogs.length;
+        for(int i = 0; i < schedules.length; i++) {
+          schedules[i] += 10;
+          if(schedules[i] % 100 >= 60) {
+            schedules[i] += 40;
+          }
+        }
+        for(int i = 0; i < timelogs.length; i++) {
+          for(int j = 0; j < timelogs[i].length; j++) {
+            int day = (j + startday) % 7;
+            if(day != 6 && day != 0) {
+              if(timelogs[i][j] - schedules[i] > 0) {
+                total--;
+                break;
+              }
             }
-
-            if (allOnTime) {
-                answer++;
-            }
+          }
         }
 
-        return answer;
-    }
-
-    private int toMinutes(int time) {
-        return (time / 100) * 60 + (time % 100);
+        return total;
     }
 }
