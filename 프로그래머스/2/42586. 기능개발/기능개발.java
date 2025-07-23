46ae119fa9a400b2a99e15b22ade87588dcd61c6
ynghan <119781387+ynghan@users.noreply.github.com>
2025-07-23 15:16:13 +0900

    [level 2] Title: 기능개발, Time: 3.26 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,47 +1,27 @@
+
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-    int[] answer = new int[progresses.length];
-
-    int totalCount = 0;
-    int idx = 0;
-    int ans_idx = 0;
-    int count = 0;
-    int length = 0;
-
-    while(totalCount < progresses.length) {
-      int quotient = (100 - progresses[idx]) / speeds[idx];
-      int remainder = (100 - progresses[idx]) % speeds[idx];
-
-      if(remainder > 0) {
-        quotient++;
-      }
-
-      for(int i = idx; i < progresses.length; i++) {
-        progresses[i] += quotient * speeds[i];
-      }
-
-      for(int i = idx; i < progresses.length; i++) {
-        if(progresses[i] >= 100) {
-          count++;
-        }
-        else {
-          break;
-        }
-      }
-
-      idx += count;
-      totalCount += count;
-      answer[ans_idx++] = count;
-      count = 0;
-      length++;
-
-    }
-
-    int[] answer2 = new int[length];
-    for (int i = 0; i < answer2.length ; i++) {
-      answer2[i] = answer[i];
-    } 
         
-        return answer2;
+        List<Integer> answer = new ArrayList<>();
+        
+        int prev = (100 - progresses[0]) % speeds[0] == 0 ? (100 - progresses[0]) / speeds[0] : (100 - progresses[0]) / speeds[0] + 1;
+        int val = 1;
+        for(int i = 1; i < progresses.length; i++) {
+            
+            int time = (100 - progresses[i]) % speeds[i] == 0 ? (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
+            
+            if(time <= prev) { 
+                val++;
+            } else {
+                answer.add(val);
+                prev = time;
+                val = 1;
+            }
+        }
+        answer.add(val);
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+        
     }
 }
