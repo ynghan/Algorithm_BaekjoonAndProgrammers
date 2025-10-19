fe20e481bf70a58566fc213bc593511ee3527493
ynghan <119781387+ynghan@users.noreply.github.com>
2025-10-19 18:06:26 +0900

    [level 2] Title: 구명보트, Time: 10.80 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length - 1;
+        int answer = 0;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
