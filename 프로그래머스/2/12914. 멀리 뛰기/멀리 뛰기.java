7fd891fc5b34262fd63cf9cb8f92a5bac93e7f71
ynghan <119781387+ynghan@users.noreply.github.com>
2025-11-14 12:00:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        long[] dp = new long[n+2];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
